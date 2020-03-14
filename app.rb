17da9660ed43930ee492d800f287c2d4e5d1f0a2
ccferraro <carmen.ferraro@kellogg.northwestern.edu>
2020-03-14 17:25:22 +0000

    3/14 before redo

--- app.rb
+++ app.rb
@@ -43,6 +43,7 @@ get "/places/:id" do
     
     @reviews = reviews_table.where(places_id: @place[:id]).to_a
     @recommend_count = reviews_table.where(places_id: @place[:id], recommend: true).count
+    @noreco_count = reviews_table.where(places_id: @place[:id], recommend: false).count
 
     view "place"
 end
@@ -69,7 +70,7 @@ post "/places/:id/reviews/create" do
         recommend: params["recommend"]
     )
 
-    redirect "/reviews/#{@place[:id]}"
+    redirect "/places/#{@place[:id]}"
 end
 
 # display the rsvp form (aka "edit")
@@ -100,4 +101,73 @@ post "/reviews/:id/update" do
     else
         view "error"
     end
+end
+
+# delete the rsvp (aka "destroy")
+get "/reviews/:id/destroy" do
+    puts "params: #{params}"
+
+    reviews = reviews_table.where(id: params["id"]).to_a[0]
+    @place = places_table.where(id: reviews[:places_id]).to_a[0]
+
+    reviews_table.where(id: params["id"]).delete
+
+    redirect "/places/#{@place[:id]}"
+end
+
+# display the signup form (aka "new")
+get "/users/new" do
+    view "new_user"
+end
+
+# receive the submitted signup form (aka "create")
+post "/users/create" do
+    puts "params: #{params}"
+
+    # if there's already a user with this email, skip!
+    existing_user = users_table.where(email: params["email"]).to_a[0]
+    if existing_user
+        view "error"
+    else
+        users_table.insert(
+            name: params["name"],
+            email: params["email"],
+            password: BCrypt::Password.create(params["password"])
+        )
+
+        redirect "/logins/new"
+    end
+end
+
+# display the login form (aka "new")
+get "/logins/new" do
+    view "new_login"
+end
+
+# receive the submitted login form (aka "create")
+post "/logins/create" do
+    puts "params: #{params}"
+
+    # step 1: user with the params["email"] ?
+    @user = users_table.where(email: params["email"]).to_a[0]
+
+    if @user
+        # step 2: if @user, does the encrypted password match?
+        if BCrypt::Password.new(@user[:password]) == params["password"]
+            # set encrypted cookie for logged in user
+            session["user_id"] = @user[:id]
+            redirect "/"
+        else
+            view "create_login_failed"
+        end
+    else
+        view "create_login_failed"
+    end
+end
+
+# logout user
+get "/logout" do
+    # remove encrypted cookie for logged out user
+    session["user_id"] = nil
+    redirect "/logins/new"
 end
