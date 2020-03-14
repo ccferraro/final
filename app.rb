31d15c96995cf36dec6ee8f26c0704331a116712
ccferraro <carmen.ferraro@kellogg.northwestern.edu>
2020-03-14 00:36:54 +0000

    edits 1

--- app.rb
+++ app.rb
@@ -14,5 +14,60 @@ before { puts; puts "--------------- NEW REQUEST ---------------"; puts }       
 after { puts; }                                                                       #
 #######################################################################################
 
-events_table = DB.from(:events)
-rsvps_table = DB.from(:rsvps)
+places_table = DB.from(:places)
+reviews_table = DB.from(:reviews)
+users_table = DB.from(:users)
+
+before do
+    @current_user = users_table.where(id: session["user_id"]).to_a[0]
+end
+
+# homepage and list of events (aka "index")
+
+get "/" do
+    puts "params: #{params}"
+
+    @places = places_table.all.to_a
+    pp @places
+
+    view "places"
+end
+
+# event details (aka "show")
+get "/places/:id" do
+    puts "params: #{params}"
+
+    @users_table = users_table
+    @place = places_table.where(id: params[:id]).to_a[0]
+    pp @place
+    
+    @reviews = reviews_table.where(places_id: @place[:id]).to_a
+    @recommend_count = reviews_table.where(places_id: @place[:id], recommend: true).count
+
+    view "place"
+end
+
+# display the rsvp form (aka "new")
+get "/places/:id/reviews/new" do
+    puts "params: #{params}"
+
+    @place = places_table.where(id: params[:id]).to_a[0]
+    view "new_review"
+end
+
+# receive the submitted rsvp form (aka "create")
+post "/places/:id/reviews/create" do
+    puts "params: #{params}"
+
+    # first find the event that rsvp'ing for
+    @place = places_table.where(id: params[:id]).to_a[0]
+    # next we want to insert a row in the reviews table with the review form data
+    reviews_table.insert(
+        place_id: @place[:id],
+        user_id: session["user_id"],
+        review: params["review"],
+        recommend: params["recommend"]
+    )
+
+    redirect "/reviews/#{@place[:id]}"
+end
--- createdb.rb
+++ createdb.rb
@@ -5,31 +5,36 @@ DB = Sequel.connect(connection_string)                                          
 #######################################################################################
 
 # Database schema - this should reflect your domain model
-DB.create_table! :events do
+DB.create_table! :places do
   primary_key :id
   String :title
   String :description, text: true
-  String :date
   String :location
 end
-DB.create_table! :rsvps do
+DB.create_table! :reviews do
   primary_key :id
-  foreign_key :event_id
-  Boolean :going
+  foreign_key :places_id
+  foreign_key :users_id
+  Boolean :recommend
   String :name
   String :email
-  String :comments, text: true
+  String :review, text: true
+end
+DB.create_table! :users do
+  primary_key :id
+  String :name
+  String :email
+  String :password
 end
 
 # Insert initial (seed) data
-events_table = DB.from(:events)
+places_table = DB.from(:places)
 
-events_table.insert(title: "Bacon Burger Taco Fest", 
-                    description: "Here we go again bacon burger taco fans, another Bacon Burger Taco Fest is here!",
-                    date: "June 21",
-                    location: "Kellogg Global Hub")
+places_table.insert(title: "Gite Le Moulin", 
+                    description: "At the foot of the famous Mont Blanc between the glaciers of Tour and Argentiere located in an old windmill you will find Le Moulin gite, the perfect place to stay for setting out on sections of your GR Tour du Mont Blanc, close to the Tour Col de Balme and a stones throw from the Aiguilles Rouges.",
+                    location: "32 Chemin du Moulin des Frasserands, 74400 Chamonix-Mont-Blanc, France")
 
-events_table.insert(title: "Kaleapolooza", 
-                    description: "If you're into nutrition and vitamins and stuff, this is the event for you.",
-                    date: "July 4",
-                    location: "Nowhere")
+places_table.insert(title: "Hotel Edelweiss", 
+                    description: "Set at the foot of Mont Blanc, this cozy, family-run mountain hotel is 8 minutes' walk from Courmayeur Mont Blanc Funivie tramway and 7 km from Pointe Helbronner mountain.",
+                    location: "Via Guglielmo Marconi, 42, 11013 Courmayeur AO, Italy")
+puts "Success!"
